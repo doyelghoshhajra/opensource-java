dc05a185631e668c64adad472544bc3efb30433f
Doyel Ghoshhajra <doyelghoshhajra@gmail.com>
2024-04-03 00:57:01 -0700

    Removed additional print statement

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("i = " + i);
-            System.out.println("This is another Print Statement ");
         }
     }
 }
