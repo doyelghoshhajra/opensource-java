f9c6bf4e254c7b46e687a051c03360a95c69b688
Doyel Ghoshhajra <doyelghoshhajra@gmail.com>
2024-04-03 11:10:47 -0700

    MyFirstHelloWorld

---
+++ src/com/hello/world/MyFirstJavaHelloWorld.java
@@ -0,0 +1,7 @@
+package com.hello.world;
+
+public class MyFirstJavaHelloWorld {
+    public static void main(String []args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
+}
